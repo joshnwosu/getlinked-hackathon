b72cea5ce76f5c88b5d2210580d720cbf1134734
joshnwosu <joshuanwosu078@gmail.com>
2023-09-20 18:56:12 +0100

    update: timeline

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -2,6 +2,7 @@ import "@mantine/core/styles.css";
 import React from "react";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "./src/theme";
+import "./src/styles/style.css";
 
 export const wrapPageElement = ({ element }) => {
   return <MantineProvider theme={theme}>{element}</MantineProvider>;
--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -1,74 +1,149 @@
-import { Box, Center, Container, Flex, Text, Title } from "@mantine/core";
+import { Box, Container, Flex, Text, Title } from "@mantine/core";
 import React from "react";
+import { timeline_data } from "./data";
 
 export default function Timeline() {
   return (
-    <Container size={"xl"}>
+    <Container size={"lg"} py={100}>
       <Flex align="center" direction="column">
-        <Title order={1}>Timeline</Title>
-        <Text w={300} ta="center" mt={"xl"}>
+        <Title order={1} style={{ color: "white" }}>
+          Timeline
+        </Title>
+        <Text w={300} ta="center" mt={"md"} style={{ color: "white" }}>
           Here is the breakdown of the time we anticipate using for the upcoming
           event.
         </Text>
       </Flex>
 
       <Box mt={100}>
-        <Flex align="center" justify="space-between">
-          <Box style={{ flex: 1 }}>
-            <Title ta="right" style={{ color: "#D434FE" }}>
-              Hackathon Announcement
-            </Title>
-            <Text ta="right" mt="md" fz="sm">
-              The getlinked tech hackathon 1.0 is formally announced to the
-              general public and teams begin to get ready to register
-            </Text>
-          </Box>
-
-          <Box
-            style={{
-              flex: 1,
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Box
+        {timeline_data.map((data, index) => {
+          return (
+            <Flex
+              key={index.toString()}
+              align="center"
+              justify="space-between"
               style={{
-                width: 50,
-                height: 50,
-                borderRadius: 50,
-                backgroundColor: "#903AFF",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                border: "5px solid transparent",
+                overflow: "hidden",
                 position: "relative",
               }}
+              py={40}
             >
               <Box
                 style={{
-                  width: 4,
-                  height: 70,
-                  backgroundColor: "#903AFF",
-                  position: "absolute",
-                  bottom: 50,
-                  zIndex: -1,
+                  flex: 1,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "flex-end",
                 }}
-              />
-              <Text size="xl" style={{ color: "white" }} fw="bolder">
-                1
-              </Text>
-            </Box>
-          </Box>
+              >
+                {index % 2 === 0 ? (
+                  <>
+                    <Title ta="right" style={{ color: "#D434FE" }} order={3}>
+                      {data.title}
+                    </Title>
+                    <Text
+                      ta="right"
+                      mt="md"
+                      fz="md"
+                      w={"80%"}
+                      maw={"100%"}
+                      style={{ color: "white" }}
+                    >
+                      {data.content}
+                    </Text>
+                  </>
+                ) : (
+                  <Title
+                    ta="right"
+                    style={{
+                      color: "#D434FE",
+                    }}
+                    order={3}
+                  >
+                    {data.date}
+                  </Title>
+                )}
+              </Box>
 
-          <Box style={{ flex: 1 }}>
-            <Text
-              style={{ color: "#D434FE", fontSize: 30, fontWeight: "bolder" }}
-            >
-              November 18, 2023
-            </Text>
-          </Box>
-        </Flex>
+              <Box
+                style={{
+                  flex: 0.3,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Box
+                  style={{
+                    width: 3,
+                    height: "100%",
+                    backgroundColor: "#903AFF",
+                    position: "absolute",
+                    top:
+                      index === 0
+                        ? 100
+                        : index === timeline_data.length - 1
+                        ? -100
+                        : 0,
+                    zIndex: -1,
+                  }}
+                />
+                <Box
+                  style={{
+                    width: 50,
+                    height: 50,
+                    borderRadius: 50,
+                    backgroundColor: "#903AFF",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    border: "10px solid #150E28",
+                    position: "relative",
+                  }}
+                >
+                  <Text size="md" style={{ color: "white" }} fw={500}>
+                    {index + 1}
+                  </Text>
+                </Box>
+              </Box>
+
+              <Box
+                style={{
+                  flex: 1,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "flex-start",
+                }}
+              >
+                {index % 2 === 0 ? (
+                  <Title
+                    style={{
+                      color: "#D434FE",
+                    }}
+                    order={3}
+                  >
+                    {data.date}
+                  </Title>
+                ) : (
+                  <>
+                    <Title style={{ color: "#D434FE" }} order={3}>
+                      {data.title}
+                    </Title>
+                    <Text
+                      mt="md"
+                      fz="md"
+                      w={"80%"}
+                      maw={"100%"}
+                      style={{ color: "white" }}
+                    >
+                      {data.content}
+                    </Text>
+                  </>
+                )}
+              </Box>
+            </Flex>
+          );
+        })}
       </Box>
     </Container>
   );
